215770dc37b8ca4bb694cddbe00420fca6dc876a
Memo Khoury <majd.khoury93@gmail.com>
2018-09-24 12:02:46 -0400

    feat: configured leveldbSandbox.js to persist data locally

--- levelSandbox.js
+++ levelSandbox.js
@@ -7,31 +7,44 @@ const chainDB = './chaindata';
 const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
-function addLevelDBData(key,value){
-  db.put(key, value, function(err) {
-    if (err) return console.log('Block ' + key + ' submission failed', err);
+function addLevelDBData(key, value) {
+  return new Promise(function (resolve, reject) {
+    db.put(key, value)
+      .on('error', function (err) {
+        reject(err);
+      }).on('close', function () {
+        resolve(db);
+      })
   })
 }
 
 // Get data from levelDB with key
-function getLevelDBData(key){
-  db.get(key, function(err, value) {
-    if (err) return console.log('Not found!', err);
-    console.log('Value = ' + value);
-  })
+function getLevelDBData(key) {
+  return new Promise(function (resolve, reject) {
+    db.get(key)
+      .on('error', function (err) {
+        reject(err);
+      }).on('close', function () {
+        resolve(db);
+      })
+  });
 }
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
-    let i = 0;
-    db.createReadStream().on('data', function(data) {
-          i++;
-        }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function() {
-          console.log('Block #' + i);
-          addLevelDBData(i, value);
-        });
+  let i = 0;
+  return new Promise(function (resolve, reject) {
+    db.createReadStream()
+      .on('data', function (data) {
+        i++;
+      }).on('error', function (err) {
+        reject(err);
+      }).on('close', function () {
+        console.log('Block #' + i);
+        addLevelDBData(i, value);
+        resolve(db);
+      });
+  });
 }
 
 /* ===== Testing ==============================================================|
@@ -46,7 +59,7 @@ function addDataToLevelDB(value) {
 |  ===========================================================================*/
 
 
-(function theLoop (i) {
+(function theLoop(i) {
   setTimeout(function () {
     addDataToLevelDB('Testing data');
     if (--i) theLoop(i);
