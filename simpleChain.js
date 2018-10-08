bb047cfd6bc06a6b627b923deb8f8a28a0513d2d
Memo Khoury <majd.khoury93@gmail.com>
2018-10-08 18:30:49 -0400

    sixth submission

--- simpleChain.js
+++ simpleChain.js
@@ -19,7 +19,7 @@ class Block {
 }
 
 /* ===== Blockchain Class ==========================
-|  Class with a constructor for new blockchain 		|
+|  Class with a constructor for new blockchain  		|
 |  ================================================*/
 
 class Blockchain {
@@ -43,6 +43,18 @@ class Blockchain {
     })
   }
 
+  getBlockHeight() {
+    return new Promise(function (resolve, reject) {
+      level.getBlockHeight()
+        .then(function (value) {
+          resolve(value);
+        })
+        .catch(function (error) {
+          reject(error);
+        })
+    })
+  }
+
   // Add new block
   addBlock(newBlock) {
     return new Promise(function (resolve, reject) {
@@ -50,8 +62,6 @@ class Blockchain {
       if (newBlock.body != "First block in the chain - Genesis block") {
         console.log("-------------------");
         console.log("Adding new block");
-        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-        console.log("Hash: " + newBlock.hash);
         console.log("Data: " + newBlock.body);
         newBlock.time = new Date().getTime().toString().slice(0, -3);
         console.log("Time: " + newBlock.time);
@@ -66,6 +76,10 @@ class Blockchain {
                 value = JSON.parse(value);
                 newBlock.previousBlockHash = value.hash;
                 console.log("New block's previous hash is: " + newBlock.previousBlockHash);
+              })
+              .then(function () {
+                newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+                console.log("Hash: " + newBlock.hash);
               })
               .then(function () {
                 console.log("about to add the new block to the chain");
@@ -89,12 +103,12 @@ class Blockchain {
       } else {
         console.log("-------------------");
         console.log("Adding genesis block");
-        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-        console.log("Hash: " + newBlock.hash);
         console.log("Height: " + newBlock.height);
         console.log("Data: " + newBlock.body);
         console.log("Time: " + newBlock.time);
         console.log("Previous block hash:" + newBlock.previousBlockHash);
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+        console.log("Hash: " + newBlock.hash);
         level.addLevelDBData(newBlock.height, JSON.stringify(newBlock).toString()).then(value => {
           resolve(value);
         });
@@ -108,7 +122,7 @@ class Blockchain {
   validateBlock(blockHeight) {
     return new Promise(function (resolve, reject) {
       // get block object
-      const block = "";
+      let block = "";
       level.getBlock(blockHeight)
         .then(value => {
           block = value;
@@ -125,39 +139,43 @@ class Blockchain {
             resolve(false);
           }
         })
+        .catch(function (error) {
+          reject(error);
+        })
     })
   }
 
   // Validate blockchain
   validateChain() {
-    let errorLog = [];
-    getBlockHeight().then(function (value) {
-      for (var i = 0; i < value - 1; i++) {
-        // validate block
-        validateBlock(i).then(function (booleanValue) {
-          if (booleanValue === false) errorLog.push(i);
-          // compare blocks hash link
-          level.getBlock(i).then(function (block) {
-            let parsedBlockOne = JSON.parse(block);
-            let hash = parsedBlock.hash;
-            level.getBlock(i + 1).then(function (block) {
-              let parsedBlockTwo = JSON.parse(block);
-              let previousHash = parsedBlockTwo.hash;
-              if (hash !== previousHash) {
-                errorLog.push(i);
-              }
+    return new Promise(function (resolve, reject) {
+      let errorLog = [];
+      level.getBlockHeight().then(function (value) {
+        for (var i = 0; i < value - 1; i++) {
+          // validate block
+          validateBlock(i).then(function (booleanValue) {
+            if (booleanValue === false) errorLog.push(i);
+            // compare blocks hash link
+            level.getBlock(i).then(function (block) {
+              let parsedBlockOne = JSON.parse(block);
+              let hash = parsedBlockOne.hash;
+              level.getBlock(i + 1).then(function (block) {
+                let parsedBlockTwo = JSON.parse(block);
+                let previousHash = parsedBlockTwo.hash;
+                if (hash !== previousHash) {
+                  errorLog.push(i);
+                }
+              })
             })
           })
-        })
-      }
-      if (errorLog.length > 0) {
-        console.log('Block errors = ' + errorLog.length);
-        console.log('Blocks: ' + errorLog);
-      } else {
-        console.log('No errors detected');
-      }
-    })
-
+        }
+        if (errorLog.length > 0) {
+          console.log('Block errors = ' + errorLog.length);
+          console.log('Blocks: ' + errorLog);
+        } else {
+          console.log('No errors detected');
+        }
+      })
+    });
   }
 }
 
@@ -170,7 +188,13 @@ let myBlockChain = new Blockchain();
     myBlockChain.addBlock(blockTest).then((result) => {
       console.log(result);
       i++;
-      if (i < 10) theLoop(i);
+      if (i < 5) theLoop(i);
     });
-  }, 10000);
+  }, 2000);
 })(0);
+
+/** commands for node: 
+ * 
+ * blockchain = new Blockchain();
+ * blockchain.addBlock("memo");
+ */
